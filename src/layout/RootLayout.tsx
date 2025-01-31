482acc9d70956ed154bc243a18ae3e09061f35ac
Abdi714 <loucheabdji@gmail.com>
2025-02-01 00:22:29 +0100

    fix du probleme de largeur

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -6,46 +6,68 @@ import Sidebar from "./Sidebar";
 
 export const RootLayout = () => {
   return (
-    <Box width="full" margin="0px" padding="0px">
-      <Grid
-        templateAreas={`"nav header"
+    <Grid
+      templateAreas={`"nav header"
                         "nav main"
                         "nav footer"`}
-        gridTemplateRows={"70px 1fr 50px"}
-        gridTemplateColumns={"299px 1fr"}
-        height="100vh"
-        gap="0.5"
-        color="blackAlpha.700"
-        fontWeight="bold"
+      gridTemplateRows={"70px 1fr 50px"}
+      gridTemplateColumns={"299px 1fr"}
+      height="100vh"
+      gap="0.5"
+      color="blackAlpha.700"
+      fontWeight="bold"
+      overflow="hidden"
+      pr="5px"
+    >
+      <GridItem
+        area={"header"}
+        background="white"
+        top="0"
+        left="0"
+        zIndex="1"
+        height="70px"
         width="100%"
+        maxWidth="1200px"
+        border="1px solid green"
+        position="sticky"
+        margin="0 auto"
+        overflow="hidden"
       >
-        <GridItem
-          area={"header"}
-          background="white"
-          position="relative"
-          top="0"
-          zIndex="1"
-          height="70px"
-          width="auto"
-          border="1px solid green"
-        >
-          <Header />
-        </GridItem>
-        <GridItem area={"nav"} background="primary.dogerBlue.101">
-          <Sidebar />
-        </GridItem>
-        <GridItem
-          area={"main"}
-          paddingLeft="2"
-          border="1px solid red"
-          borderColor="gray.500"
-        >
-          <Outlet />
-        </GridItem>
-        <GridItem paddingLeft="2" area={"footer"} border="1px solid black">
-          <Footer />
-        </GridItem>
-      </Grid>
-    </Box>
+        <Header />
+      </GridItem>
+      <GridItem
+        area={"nav"}
+        background="primary.dogerBlue.101"
+        position="sticky"
+        left="0"
+      >
+        <Sidebar />
+      </GridItem>
+      <GridItem
+        area={"main"}
+        paddingLeft="2"
+        border="1px solid red"
+        borderColor="gray.500"
+        w="100%"
+        maxWidth="1200px"
+        margin="0 auto"
+        padding="20px"
+        overflowY="auto"
+        scrollbarWidth="none"
+      >
+        <Outlet />
+      </GridItem>
+      <GridItem
+        paddingLeft="2"
+        area={"footer"}
+        border="1px solid black"
+        w="100%"
+        maxWidth="1200px"
+        margin="0 auto"
+        scrollbarWidth="none"
+      >
+        <Footer />
+      </GridItem>
+    </Grid>
   );
 };
