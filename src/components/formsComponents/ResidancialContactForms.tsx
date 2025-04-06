6e5ecc4749dc7e7ec77fedd259d5c4cf864bbada
Abdi714 <loucheabdji@gmail.com>
2025-04-06 17:04:50 +0200

    Creation des formulaire

---
+++ src/components/formsComponents/ResidancialContactForms.tsx
@@ -0,0 +1,37 @@
+import { FormFieldSet } from "./FormFieldSet";
+import { InputTextField } from "../customFormFields/InputTextField";
+
+export const ResidancialContactForms = () => {
+  return (
+    <>
+      <FormFieldSet label="Adresse et contact">
+        <InputTextField label="Code postale  " placeholder="Code postale " />
+        <InputTextField label="Localité " placeholder="Localité " />
+        <InputTextField label="Domicile " placeholder="Domicile" />
+        <InputTextField
+          label="Situation logement"
+          placeholder="Situation logement "
+        />
+      </FormFieldSet>
+
+      <FormFieldSet label="Contact et email">
+        <InputTextField label="Mobile " placeholder="Mobile" />
+        <InputTextField label="Adresse email " placeholder="Adresse email " />
+      </FormFieldSet>
+      <FormFieldSet label="Statut de résidence ">
+        <InputTextField
+          label="Pays d’adresse fiscale"
+          placeholder="Pays d’adresse fiscale"
+        />
+        <InputTextField
+          label="Statut résidence "
+          placeholder="Statut résidence"
+        />
+        <InputTextField
+          label="Date d’entrée territoire "
+          placeholder="Date d’entrée territoire "
+        />
+      </FormFieldSet>
+    </>
+  );
+};
