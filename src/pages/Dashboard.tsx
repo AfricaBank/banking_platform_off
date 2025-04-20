93ea3d8f34d524eb0d9dc53b14b13f5746976e9b
mor mbathie <mormbathie98@gmail.com>
2025-04-20 14:48:50 +0100

    Ajout du composant GeneralInfoBlock

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,9 @@
 import { CustomDatePicker } from "@/components/customFormFields/CustomDatePicker";
-
+import BlocInfosGenerales from "@/components/blocInfos/BlocInfosGenerales";
 export const Dashboard = () => {
   return (
     <>
+    <BlocInfosGenerales/>
       <div>
         je suis dashboard
         <CustomDatePicker />
