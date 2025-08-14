411a895ef565191aa2e11ad950902013e52d9f74
Abdi714 <loucheabdji@gmail.com>
2025-08-14 21:58:33 +0100

    fix du probleme du dropdown list

--- src/components/customFormFields/DropDownList.tsx
+++ src/components/customFormFields/DropDownList.tsx
@@ -1,51 +1,56 @@
 "use client";
 import { Portal, Select } from "@chakra-ui/react";
 import { ComponentProps } from "react";
+import { collectionList } from "@/dataObject/ListCollection.ts";
 
 interface DropDownListProps {
-    highlightColor?: string;
-    withIndicator?: boolean;
-    label: string;
-    items: { label: string; value: string }[];
+  highlightColor?: string;
+  withIndicator?: boolean;
+  label: string;
+  items?: { label: string; value: string }[];
 }
 
 type MySelectProps = ComponentProps<typeof Select.Root> & DropDownListProps;
 
 export const DropDownList = ({
-                                 highlightColor = "blue",
-                                 withIndicator = true,
-                                 label,
-                                 items,
-                                 ...chakraProps
-                             }: MySelectProps) => {
-    return (
-        <Select.Root {...chakraProps} size="md" width="30%" colorPalette="gray">
-            <Select.HiddenSelect />
-            <Select.Label color="#6E7C7C">{label}</Select.Label>
-            <Select.Control>
-                <Select.Trigger>
-                    <Select.ValueText placeholder={label} color="#6E7C7C" />
-                </Select.Trigger>
-                {withIndicator && (
-                    <Select.IndicatorGroup>
-                        <Select.Indicator />
-                    </Select.IndicatorGroup>
-                )}
-            </Select.Control>
-            <Portal>
-                <Select.Positioner>
-                    <Select.Content>
-                        {items.map((item) => (
-                            <Select.Item item={item.value} key={item.value}>
-                <span style={{ color: highlightColor }}>
-                  {item.label}
-                </span>
-                                {withIndicator && <Select.ItemIndicator />}
-                            </Select.Item>
-                        ))}
-                    </Select.Content>
-                </Select.Positioner>
-            </Portal>
-        </Select.Root>
-    );
+  highlightColor = "blue",
+  withIndicator = true,
+  label,
+  items,
+  ...chakraProps
+}: MySelectProps) => {
+  return (
+    <Select.Root
+      {...chakraProps}
+      size="md"
+      width="30%"
+      colorPalette="gray"
+      collection={collectionList}
+    >
+      <Select.HiddenSelect />
+      <Select.Label color="#6E7C7C">{label}</Select.Label>
+      <Select.Control>
+        <Select.Trigger>
+          <Select.ValueText placeholder={label} color="#6E7C7C" />
+        </Select.Trigger>
+        {withIndicator && (
+          <Select.IndicatorGroup>
+            <Select.Indicator />
+          </Select.IndicatorGroup>
+        )}
+      </Select.Control>
+      <Portal>
+        <Select.Positioner>
+          <Select.Content>
+            {collectionList.items.map((item) => (
+              <Select.Item item={item} key={item.value}>
+                <span style={{ color: highlightColor }}>{item.label}</span>
+                {withIndicator && <Select.ItemIndicator />}
+              </Select.Item>
+            ))}
+          </Select.Content>
+        </Select.Positioner>
+      </Portal>
+    </Select.Root>
+  );
 };
